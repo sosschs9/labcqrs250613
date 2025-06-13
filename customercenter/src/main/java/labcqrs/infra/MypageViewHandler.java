1c0608b1e6a25d294d6ac277d3409cf7593cc3e3
hye_gooong <81686317+sosschs9@users.noreply.github.com>
2025-06-13 15:34:23 +0900

    commit

---
+++ customercenter/src/main/java/labcqrs/infra/MypageViewHandler.java
@@ -0,0 +1,86 @@
+package labcqrs.infra;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
+import labcqrs.config.kafka.KafkaProcessor;
+import labcqrs.domain.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MypageViewHandler {
+
+    //<<< DDD / CQRS
+    @Autowired
+    private MypageRepository mypageRepository;
+
+    @StreamListener(KafkaProcessor.INPUT)
+    public void whenOrderPlaced_then_CREATE_1(
+        @Payload OrderPlaced orderPlaced
+    ) {
+        try {
+            if (!orderPlaced.validate()) return;
+
+            // view 객체 생성
+            Mypage mypage = new Mypage();
+            // view 객체에 이벤트의 Value 를 set 함
+            mypage.setOrderId(orderPlaced.getId());
+            mypage.setProductId(orderPlaced.getProductId());
+            mypage.setOrderStatus("주문완료");
+            // view 레파지 토리에 save
+            mypageRepository.save(mypage);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @StreamListener(KafkaProcessor.INPUT)
+    public void whenDeliveryStarted_then_UPDATE_1(
+        @Payload DeliveryStarted deliveryStarted
+    ) {
+        try {
+            if (!deliveryStarted.validate()) return;
+            // view 객체 조회
+            Optional<Mypage> mypageOptional = mypageRepository.findByOrderId(
+                deliveryStarted.getOrderId()
+            );
+
+            if (mypageOptional.isPresent()) {
+                Mypage mypage = mypageOptional.get();
+                // view 객체에 이벤트의 eventDirectValue 를 set 함
+                mypage.setDeliveryStatus("배송시작");
+                // view 레파지 토리에 save
+                mypageRepository.save(mypage);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @StreamListener(KafkaProcessor.INPUT)
+    public void whenOrderCancelled_then_UPDATE_2(
+        @Payload OrderCancelled orderCancelled
+    ) {
+        try {
+            if (!orderCancelled.validate()) return;
+            // view 객체 조회
+            Optional<Mypage> mypageOptional = mypageRepository.findByOrderId(
+                orderCancelled.getId()
+            );
+
+            if (mypageOptional.isPresent()) {
+                Mypage mypage = mypageOptional.get();
+                // view 객체에 이벤트의 eventDirectValue 를 set 함
+                mypage.setOrderStatus("주문취소");
+                // view 레파지 토리에 save
+                mypageRepository.save(mypage);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    //>>> DDD / CQRS
+}
